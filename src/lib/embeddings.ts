506e1001b9c8d7a506efe8f8fadc4a8a6b8e7738
Atif Ali <atif@monotote.com>
2023-09-29 01:04:34 +0500

    first commit

---
+++ src/lib/embeddings.ts
@@ -0,0 +1,23 @@
+import {Configuration, OpenAIApi} from "openai-edge";
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(config);
+
+export async function getEmbeddings(text: string) {
+    try {
+        const response = await openai.createEmbedding({
+            model: "text-embedding-ada-002",
+            input: text.replace(/\n/g, ' '),
+        });
+        console.log(response);
+        const result = await response.json();
+        console.log(result.data[0].embedding as number[])
+        return result.data[0].embedding as number[];
+    } catch (error) {
+        console.log("error calling openai embeddings api", error);
+        throw error;
+    }
+}
